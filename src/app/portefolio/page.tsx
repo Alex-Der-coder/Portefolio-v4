6b53a1b4e94bc040d0ef364000a140be2b14b419
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2024-09-24 21:30:17 +0200

    feat : add projet

--- src/app/portefolio/page.tsx
+++ src/app/portefolio/page.tsx
@@ -2,63 +2,18 @@ import React, { Suspense } from "react";
 import WhoIAm from "../_components/WhoIAm";
 import Techno from "../_components/Techno";
 import AccordionDemo from "../_components/AccordionDemo";
+import { GET } from "../api/Projet/route";
 
+export default async function Portfolio() {
+  const response = await GET();
 
-
-// Fetching data in a server component
-async function fetchData() {
-  try {
-    const res = await fetch('http://localhost:3000/api/Projet');
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    return res.json();
-  } catch (error) {
-    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred');
-  }
-}
-
-interface Resource {
-  asset_id: string;
-  public_id: string;
-  folder: string;
-  filename: string;
-  format: string;
-  version: number;
-  resource_type: string;
-  type: string;
-  created_at: string;
-  uploaded_at: string;
-  bytes: number;
-  backup_bytes: number;
-  width: number;
-  height: number;
-  aspect_ratio: number;
-  pixels: number;
-  url: string;
-  secure_url: string;
-  status: string;
-  access_mode: string;
-  access_control: any;
-  etag: string;
-  created_by: any;
-  uploaded_by: any;
-}
-
-// Définition de l'interface pour l'objet principal
-interface Results {
-  total_count: number;
-  time: number;
-  resources: Resource[];
-}
-
-export default async function Portefolio() {
-  const data = await fetchData();
-  
-
+  // Parser la réponse JSON pour obtenir les projets
+  const data = await response.json();  // Le NextResponse est converti en JSON ici
+  console.log('Données récupérées', data);
   return (
     <>
       <WhoIAm />
+      {/* Passage des données à la prop data */}
       <Techno data={data} />
       <AccordionDemo />
     </>
